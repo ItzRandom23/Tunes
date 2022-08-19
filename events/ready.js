5c1210f9f5d7ccc665aef4517abef69fa28237f5
Cool Management <100831398+CoolMusic1@users.noreply.github.com>
2022-08-19 13:47:04 +0530

    Delete events directory

--- events/ready.js
+++
@@ -1,9 +0,0 @@
-const client = require("..");
-
-client.on('ready', () => {
-    console.log(`${client.user.username} Is Online`);
-    client.user.setActivity({
-        name : `@${client.user.username} /play`,
-        type : "PLAYING",
-    })
-})
