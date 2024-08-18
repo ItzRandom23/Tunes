01b3e59cb745d305a4908b1d311fe08ec8a3272c
CPS_GAMING#0 <149692687+CPS7@users.noreply.github.com>
2024-08-18 20:41:27 +0530

    Update previous.js

--- src/commands/Slash/Music/previous.js
+++ src/commands/Slash/Music/previous.js
@@ -1,1 +1,27 @@
+const { EmbedBuilder } = require("discord.js");
 
+module.exports = {
+  name: "previous",
+  description: "replays the current track",
+  settings: {
+    ownerOnly: false,
+    inVoiceChannel: true,
+    sameVoiceChannel: true,
+    musicnotplaying: true,
+    musicplaying: true,
+  },
+  /**
+   * @param {{ client: import("../../../structures/Client"), interaction: import("discord.js").CommandInteraction }}
+   */
+  run: async (client, interaction, player) => {
+    player.previous();
+
+    return interaction.editReply({
+      embeds: [
+        new EmbedBuilder()
+          .setColor("Blue")
+          .setDescription("played the previous track."),
+      ],
+    });
+  },
+};
