06148a65744937fa171207802448d323fa05693b
CPS_GAMING#0 <149692687+CPS7@users.noreply.github.com>
2024-08-18 21:17:04 +0530

    Update skip.js

--- src/commands/Message/Music/skip.js
+++ src/commands/Message/Music/skip.js
@@ -1,1 +1,46 @@
+const { EmbedBuilder } = require("discord.js");
 
+module.exports = {
+  name: "skip",
+  permission: "",
+  description: "plays the next track",
+  usage: "",
+  settings: {
+    ownerOnly: false,
+    inVoiceChannel: true,
+    sameVoiceChannel: true,
+    musicnotplaying: true,
+    musicplaying: true,
+  },
+  /**
+   * @param {{ message: import("discord.js").Message }}
+   */
+  run: async ({ message, player }) => {
+    if (!player.playing) {
+      return message.channel.send({
+      embeds: [
+        new EmbedBuilder()
+          .setColor("Red")
+          .setDescription("I am not playing anything right now."),
+      ],
+    });
+    }
+
+    if (player.queue.size === 0) {
+            const noMoreTracksEmbed = new EmbedBuilder()
+                .setColor('#FF0000')
+                .setDescription('There are no more tracks in the queue to skip to.');
+            return await message.channel.send({ embeds: [noMoreTracksEmbed] });
+    }
+    
+    player.stop();
+
+    return message.channel.send({
+      embeds: [
+        new EmbedBuilder()
+          .setColor("Blue")
+          .setDescription("skipped to the next track."),
+      ],
+    });
+  },
+};
