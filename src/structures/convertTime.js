55b02889e0840eb20b89cbfee4a38099b72f454f
Kenver123 <165576302+Kenver123@users.noreply.github.com>
2025-01-12 09:42:45 +0530

    Add files via upload

---
+++ src/structures/convertTime.js
@@ -0,0 +1,20 @@
+module.exports = {
+
+    convertTime: function (duration) {
+        var milliseconds = parseInt((duration % 1000) / 100),
+            seconds = parseInt((duration / 1000) % 60),
+            minutes = parseInt((duration / (1000 * 60)) % 60),
+            hours = parseInt((duration / (1000 * 60 * 60)) % 24);
+
+        hours = hours < 10 ? "0" + hours : hours;
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        if (duration < 3600000) {
+            return minutes + ":" + seconds;
+        } else {
+            return hours + ":" + minutes + ":" + seconds;
+        }
+    }
+
+}
